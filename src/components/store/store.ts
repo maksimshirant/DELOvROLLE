e42451b666e84c11804a37220b615058b3afc132
Shirant Maksim <shirant.maksim@yandex.ru>
2024-08-12 22:53:41 +0300

    поднят сервер, сделана декомпозиция

--- src/components/store/store.ts
+++ src/components/store/store.ts
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { rollsReducer } from './rollsSlice';
 
+
+
 export const store = configureStore({
    reducer: {
       rolls: rollsReducer,
    },
 });
+
 
 export type RootState = ReturnType<typeof store.getState>;
---
+++ src/components/types/productsType.ts
@@ -0,0 +1,10 @@
+export type Products = {
+   id: number,
+   name: string,
+   weight: string,
+   ingredients: string[],
+   price: string,
+   img: string,
+   much: string,
+   legend: string
+};
